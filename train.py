f2de5420c03d151a46b8ca927d0318e1265d660b
Shirshak <shirshak.acharya@gmail.com>
2024-04-20 22:20:57 +0545

    added code for validation and train transforms

---
+++ train.py
@@ -0,0 +1,59 @@
+import os 
+import matplotlib.pyplot as plt
+from monai.apps import DecathlonDataset
+from monai.losses import DiceLoss
+from monai.metrics import DiceMetric
+from monai.transforms import (Activations,Activationsd,AsDiscrete,AsDiscreted,Compose,Invertd,LoadImaged,NormalizeIntensityd,Orientationd,RandFlipd,RandScaleIntensityd,andShiftIntensityd,RandSpatialCropd,Spacingd,EnsureTyped,EnsureChannelFirstd)
+
+import torch 
+import tempfile
+from utils import ConvertToMultiChannelBasedOnBratsClassesd
+
+train_transform = Compose(
+    [
+        # load 4 Nifti images and stack them together
+        LoadImaged(keys=["image", "label"]),
+        EnsureChannelFirstd(keys="image"),
+        EnsureTyped(keys=["image", "label"]),
+        ConvertToMultiChannelBasedOnBratsClassesd(keys="label"),
+        Orientationd(keys=["image", "label"], axcodes="RAS"),
+        Spacingd(
+            keys=["image", "label"],
+            pixdim=(1.0, 1.0, 1.0),
+            mode=("bilinear", "nearest"),
+        ),
+        RandSpatialCropd(keys=["image", "label"], roi_size=[224, 224, 144], random_size=False),
+        RandFlipd(keys=["image", "label"], prob=0.5, spatial_axis=0),
+        RandFlipd(keys=["image", "label"], prob=0.5, spatial_axis=1),
+        RandFlipd(keys=["image", "label"], prob=0.5, spatial_axis=2),
+        NormalizeIntensityd(keys="image", nonzero=True, channel_wise=True),
+        RandScaleIntensityd(keys="image", factors=0.1, prob=1.0),
+        RandShiftIntensityd(keys="image", offsets=0.1, prob=1.0),
+    ]
+)
+
+val_transform = Compose(
+    [
+        LoadImaged(keys=["image", "label"]),
+        EnsureChannelFirstd(keys="image"),
+        EnsureTyped(keys=["image", "label"]),
+        ConvertToMultiChannelBasedOnBratsClassesd(keys="label"),
+        Orientationd(keys=["image", "label"], axcodes="RAS"),
+        Spacingd(
+            keys=["image", "label"],
+            pixdim=(1.0, 1.0, 1.0),
+            mode=("bilinear", "nearest"),
+        ),
+        NormalizeIntensityd(keys="image", nonzero=True, channel_wise=True),
+    ]
+)
+
+
+if __name__ == "__main__":
+    directory = os.environ.get("MONAI_DATA_DIRECTORY")
+    root_dir = tempfile.mkdtemp() if directory is None else directory
+    print(root_dir)
+
+
+
+
