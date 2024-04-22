6b0d084ad62f15ad5a86e15931a27a280fa3c629
Shirshak <shirshak.acharya@gmail.com>
2024-04-22 10:43:06 +0545

    adding dataloader and dataset

---
+++ data.py
@@ -0,0 +1,74 @@
+from utils import ConvertToMultiChannelBasedOnBratsClassesd
+from monai.transforms import (Activations,Activationsd,AsDiscrete,AsDiscreted,Compose,Invertd,LoadImaged,NormalizeIntensityd,Orientationd,RandFlipd,RandScaleIntensityd,RandSpatialCropd,Spacingd,EnsureTyped,EnsureChannelFirstd,RandShiftIntensityd)
+import matplotlib.pyplot as plt
+from monai.apps import DecathlonDataset
+from monai.losses import DiceLoss
+from monai.metrics import DiceMetric
+from monai.data import DataLoader
+import torch 
+
+
+train_transform = Compose(
+    [
+        # load 4 Nifti images and stack them together
+        LoadImaged(keys=["image", "label"]),
+        EnsureChannelFirstd(keys="image"),
+        EnsureTyped(keys=["image", "label"]),
+
+        ConvertToMultiChannelBasedOnBratsClassesd(keys="label"),
+        
+        Orientationd(keys=["image", "label"], axcodes="RAS"),
+        Spacingd(
+            keys=["image", "label"],
+            pixdim=(1.0, 1.0, 1.0),
+            mode=("bilinear", "nearest"),
+        ),
+        RandSpatialCropd(keys=["image", "label"], roi_size=[224, 224, 144], random_size=False),
+        RandFlipd(keys=["image", "label"], prob=0.5, spatial_axis=0),
+        RandFlipd(keys=["image", "label"], prob=0.5, spatial_axis=1),
+        RandFlipd(keys=["image", "label"], prob=0.5, spatial_axis=2),
+        NormalizeIntensityd(keys="image", nonzero=True, channel_wise=True),
+        RandScaleIntensityd(keys="image", factors=0.1, prob=1.0),
+        RandShiftIntensityd(keys="image", offsets=0.1, prob=1.0),
+    ]
+)
+val_transform = Compose(
+    [
+        LoadImaged(keys=["image", "label"]),
+        EnsureChannelFirstd(keys="image"),
+        EnsureTyped(keys=["image", "label"]),
+        ConvertToMultiChannelBasedOnBratsClassesd(keys="label"),
+        Orientationd(keys=["image", "label"], axcodes="RAS"),
+        Spacingd(
+            keys=["image", "label"],
+            pixdim=(1.0, 1.0, 1.0),
+            mode=("bilinear", "nearest"),
+        ),
+        NormalizeIntensityd(keys="image", nonzero=True, channel_wise=True),
+    ]
+)
+
+def get_data(root_dir):
+
+    train_ds = DecathlonDataset(
+        root_dir=root_dir,
+        task="Task01_BrainTumour",
+        transform=train_transform,
+        section="training",
+        download=True,
+        cache_rate=0.0,
+        num_workers=4)
+
+    train_loader = DataLoader(train_ds, batch_size=1, shuffle=True, num_workers=4)
+
+    val_ds = DecathlonDataset(
+        root_dir=root_dir,
+        task="Task01_BrainTumour",
+        transform=val_transform,
+        section="validation",
+        download=False,
+        cache_rate=0.0,
+        num_workers=4)
+    val_loader = DataLoader(val_ds, batch_size=1, shuffle=False, num_workers=4)
+
+    return train_loader, val_loader
